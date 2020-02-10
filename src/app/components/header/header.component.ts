15a87b417f2a9711fae62d5917d06c1d4c83370a
Edwin Serna <edwinflow888@gmail.com>
2020-02-09 22:36:50 -0500

    Agregando botones de registro

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  public isLogget:boolean = false;
+  public isLogget = false;
 
   constructor() {}
 
